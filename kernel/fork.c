fa5cb352d945e59a320305e993fd49afa02115f2
jmjung <jmjung@hanyang.ac.kr>
2016-03-21 17:21:08 +0900

    Modify blkdev.h

--- kernel/fork.c
+++ kernel/fork.c
@@ -124,7 +124,17 @@ static struct kmem_cache *task_struct_cachep;
 
 static inline struct task_struct *alloc_task_struct_node(int node)
 {
-	return kmem_cache_alloc_node(task_struct_cachep, GFP_KERNEL, node);
+	/* UFS */
+	struct task_struct *p;
+	p = kmem_cache_alloc_node(task_struct_cachep, GFP_KERNEL, node);
+	p->epoch = 0;
+	p->barrier_tail = 0;
+	p->epoch_fail = 0;
+	//LIST_HEAD_INIT(&p->epoch_pending);
+	//LIST_HEAD_INIT(&p->epoch_complete);
+	//LIST_HEAD_INIT(&p->epoch_epoch_error);
+	return p;
+	//return kmem_cache_alloc_node(task_struct_cachep, GFP_KERNEL, node);
 }
 
 static inline void free_task_struct(struct task_struct *tsk)
