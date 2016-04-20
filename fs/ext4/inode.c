5916597d310ee401d80d5e0db648e105dbaa823c
jmjung <jmjung@hanyang.ac.kr>
2016-04-20 23:32:40 +0900

    Fix missing write in EXT4 with delayed allocation. Need to verify.

--- fs/ext4/inode.c
+++ fs/ext4/inode.c
@@ -2291,7 +2291,7 @@ static int write_cache_pages_da(handle_t *handle,
 	index = wbc->range_start >> PAGE_CACHE_SHIFT;
 	end = wbc->range_end >> PAGE_CACHE_SHIFT;
 
-	if (wbc->sync_mode == WB_SYNC_ALL || wbc->tagged_writepages)
+	if (wbc->sync_mode == WB_SYNC_ALL /* UFS */ || wbc->sync_mode == WB_ORDERED_ALL || wbc->sync_mode == WB_BARRIER_ALL || wbc->tagged_writepages)
 		tag = PAGECACHE_TAG_TOWRITE;
 	else
 		tag = PAGECACHE_TAG_DIRTY;
@@ -2524,6 +2524,9 @@ static int ext4_da_writepages(struct address_space *mapping,
 retry:
 	if (wbc->sync_mode == WB_SYNC_ALL || wbc->tagged_writepages)
 		tag_pages_for_writeback(mapping, index, end);
+	/* UFS */
+	if (wbc->sync_mode == WB_ORDERED_ALL || wbc->sync_mode == WB_BARRIER_ALL)
+ 	        tag_pages_for_writeback(mapping, index, end);
 
 	blk_start_plug(&plug);
 	while (!ret && wbc->nr_to_write > 0) {
